66bcd033a3acc16ca1a9048168152f539b69e806
Masanobu Ishiyama <ishiyama@MasanobunoMacBook-Pro.local>
2024-06-12 22:38:39 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
-import cors from "cors";
 import express, { Application, Request, Response, NextFunction } from "express";
-import * as handler from "./handler";
 import * as Sentry from "@sentry/node";
 import httpContext from "express-http-context";
 
-import "./instrument";
+Sentry.init({
+  dsn: "https://f8b3c60499aaa13388753f7123b4dab8@o4507418923958272.ingest.us.sentry.io/4507419031896064",
+});
 
 const app: Application = express();
 const PORT = 3000;
 
 app.use(httpContext.middleware);
-app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use((req, res, next) => {
   const traceId = crypto.randomUUID();
@@ -19,8 +18,13 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/test1", handler.test1);
-app.get("/test2", handler.test2);
+app.get("/", (req, res, next) => {
+  try {
+    throw new Error("2024-06-12 22:37");
+  } catch (error) {
+    next(error);
+  }
+});
 
 // Sentry.setupExpressErrorHandler(app);
 
@@ -32,7 +36,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
 
   Sentry.captureException(err, (s) => {
     s.setLevel("fatal");
-    s.setTag("app-traceId", httpContext.get("traceId"))
+    s.setTag("app-traceId", httpContext.get("traceId"));
     return s;
   });
 
