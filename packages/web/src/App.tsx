89f2125784c3e8723ff10c56416b120fe5b9a20c
Masanobu Ishiyama <ishiyama@MasanobunoMacBook-Pro.local>
2024-06-12 21:44:06 +0900

    wip

---
+++ packages/web/src/App.tsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+export default function Home() {
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000")  // ローカルのバックエンドサーバーのURLにgetメソッドでアクセス
+      .then((response) => {
+        console.log(response.data.message)
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
+  }, []);
+
+  return <div></div>;
+}
